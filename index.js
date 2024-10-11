242e415a6b9875e5a6faaedfc63ad3f580ff206f
Brian <eyenani@gmail.com>
2024-10-11 15:09:03 -0600

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express= require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app=express()
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('content', (req, res) =>
      { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
