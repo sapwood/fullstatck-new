e87e1b6d5223d71a10d11c310d7a0b156eebd3ae
Brian <eyenani@gmail.com>
2024-10-13 18:17:29 -0600

    Exercise 3.21

--- index.js
+++ index.js
@@ -15,28 +15,7 @@ morgan.token('content', (req, res) =>
 const format= ':method :url :status :res[content-length] - :response-time ms :content'
 app.use(morgan(format))
 
-let persons =[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 
 
 
@@ -108,21 +87,7 @@ app.post('/api/persons',(request,response,next)=>{
     const name=request.body.name
     const number=request.body.number
 
-    // if (!(name&&number)){
-    //     return response.status(400).json({
-    //         error:'content missing'
-    //     })
-    // }
-
-
-    // const isDuplicated = persons.some(p=>(p.name.toLowerCase()===name.toLowerCase()))
-    
-
-    // if (isDuplicated){
-    //     return response.status(400).json({
-    //         error:'name must be unique'
-    //     })
-    // }
+ 
 
     const person= new People({
         name:name,
